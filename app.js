83aeecba800edafcef97107ed702515e6441b326
Timmy Schuetz <timotheusschuetz@outlook.de>
2022-04-02 19:42:03 +0200

    Follow the Signs!

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var http = require('http')
+fs = require('fs')
+port = 80
+
 http.createServer(function(req, res){
-    res.writeHead(200,{'content-Type': 'text/html'})
-    res.write("horst")
-    res.end()
-}).listen(80)
+
+    fs.readFile("index.html",function(err, data){
+        res.writeHead(200,{'content-Type': 'text/html'})
+        res.write(data)
+        res.end()
+    })
+
+    
+}).listen(port)
+console.log('running on port: '+ port)
