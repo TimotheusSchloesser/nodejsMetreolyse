6586d3a2e9dfcac98f85c0bb81c2ee4bbc2ddbd1
Timmy Schuetz <timotheusschuetz@outlook.de>
2022-04-13 19:33:01 +0200

    Https und Redirect

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-var http = require('http'),
-fs = require('fs')
-port = 80
+'use strict';
+const http = require('http')
+const https = require('https')
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const app = express()
 
-http.createServer(function(req, res){
-    if(req.url.indexOf(".css") !== -1){
-        fs.readFile(__dirname + "/css/style.css",function(err, data){
-            res.writeHead(200,{'content-Type': 'text/css'})
-            res.write(data)
-            res.end()  
-        })
-    } 
-    else{ 
-        fs.readFile(__dirname + "/index.html",function(err, data){
-            res.writeHead(200,{'content-Type': 'text/html'})
-            res.write(data)
-            res.end()  
-        })
-    }
-    
-}).listen(port)
-console.log('running on port: '+ port)
+const port = 80
+const sslPort = 443
+const sslOpt = {
+    key: fs.readFileSync("./ssl/privkey.pem"),
+    cert: fs.readFileSync("./ssl/cert.pem")
+}
+
+
+global.__basedir = __dirname
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
+http.createServer((req, res) => {
+    console.log('redirect')
+    res.writeHead(301, { "Location": "https://lyra.et-inf.fho-emden.de:20144/" })
+    res.end()
+}).listen(port, (err) => {
+    console.log("Port:" + port)
+})
+
+let server = https.createServer(sslOpt, app).listen(sslPort, (err) => {
+    console.log("ListenOnPort " + sslPort)
+})
+
+app.get('/', (req, res) => {
+    res.render("index", {
+        title: 'Hello',
+        message: 'World!'
+    })
+})
