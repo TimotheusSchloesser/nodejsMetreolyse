8be13fe9e61e07aef33a13b6afb8c22908814f76
Timmy Schuetz <timotheusschuetz@outlook.de>
2022-06-14 18:04:10 +0200

    Test von Code-Ansatz zur API-isierung

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs');
 const { exit } = require('process');
-const app = express()
+const getApp = require('./lib/getApp')
+// const database = require('./lib/database')
 // const WebSocket = require("ws")
-
+const app = getApp
 const port = 80
 const sslPort = 443
 const sslOpt = {
@@ -15,28 +16,28 @@ const sslOpt = {
     cert: fs.readFileSync("./ssl/cert.pem")
 }
 
-var MongoClient = require('mongodb').MongoClient;
-var database;
+// var MongoClient = require('mongodb').MongoClient;
+// var database;
 
 // Connection URL
 var url = 'mongodb://localhost:27017/';
 // Use connect method to connect to the Server
 
-MongoClient.connect(url, function (err, client) {
-    if (err) {
-        console.log('failed to connect', err.message)
-        process.exit(1)
-    }
+// MongoClient.connect(url, function (err, client) {
+//     if (err) {
+//         console.log('failed to connect', err.message)
+//         process.exit(1)
+//     }
 
-    var db = client.db('traindiary');
+//     var db = client.db('traindiary');
     
-    const testEntry = {
-        date : Date.now(),
-        bpm : "150",
-        stats : "90"
-    }
+//     const testEntry = {
+//         date : Date.now(),
+//         bpm : "150",
+//         stats : "90"
+//     }
 
-    const entrys = db.collection('dates')
+    // const entrys = db.collection('dates')
     
     
     // entrys.insertOne(testEntry, err => {
@@ -47,18 +48,18 @@ MongoClient.connect(url, function (err, client) {
     //     console.log('Test sucessfully')
     // })
 
-    entrys.find({date: '1.1.11'}).toArray((err, documents) => {
-        if(err) {
-           console.log('failed to find!')
-           process.exit(1) 
-        }
-        console.log(documents)
-        let dbTestDate = JSON.stringify(documents)
-        app.get('/db', function (req, res) {
-            res.render('db', { title: 'dbTest', message: dbTestDate});
-         });
-        client.close()
-    })
+    // entrys.find({date: '1.1.11'}).toArray((err, documents) => {
+    //     if(err) {
+    //        console.log('failed to find!')
+    //        process.exit(1) 
+    //     }
+    //     console.log(documents)
+    //     let dbTestDate = JSON.stringify(documents)
+    //     app.get('/db', function (req, res) {
+    //         res.render('db', { title: 'dbTest', message: dbTestDate});
+    //      });
+    //     client.close()
+    // })
 
         // entrys.find({date: '1.1.11'}).toArray((err, documents) => {
     //     if(err) {
@@ -74,20 +75,20 @@ MongoClient.connect(url, function (err, client) {
     //     console.log(result.date);
     //     client.close();
     // });
-});
+// });
 
 
-global.__basedir = __dirname
+// global.__basedir = __dirname
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'pug')
+// app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static(path.join(__dirname, "public")))
+// app.use(express.static(path.join(__dirname, "public")))
 
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+// app.use(express.json())
+// app.use(express.urlencoded({
+//     extended: true
+// }))
 
 
 http.createServer((req, res) => {
@@ -100,25 +101,4 @@ http.createServer((req, res) => {
 
 https.createServer(sslOpt, app).listen(sslPort, (err) => {
     console.log("ListenOnPort " + sslPort)
-})
-
-// const books = [{"title":"LillyFee"}]
-// const users =["sven","hio","hoi"]
-
-
-
-app.get('/', (req, res) => {
-    res.render("metrolyse")
-    title: 'Metrolyse'
-})
-
-
-app.get('/settings', (req, res) => {
-    res.render("settings")
-    title: "Settings"
-})
-
-app.get('/stuff', (req, res) => {
-    res.render("stuff")
-    title: "Stuff"
 })
