3bd99ed2d1eb739e4cd364a06f76e6643d03dd96
Timmy Schuetz <timotheusschuetz@outlook.de>
2022-06-16 14:33:58 +0200

    Nun doch zuerst die Routen in die getApp gesetzt

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 'use strict';
 const http = require('http')
 const https = require('https')
-const express = require('express')
-const path = require('path')
 const fs = require('fs');
-const { exit } = require('process');
-const app = express()
-const routes = require('./lib/routes')
+const path = require('path')
+
+// const { exit } = require('process');
+const getApp = require('./lib/getApp')
+const app = getApp()
 // const WebSocket = require("ws")
+
 
 const port = 80
 const sslPort = 443
@@ -16,79 +17,66 @@ const sslOpt = {
     cert: fs.readFileSync("./ssl/cert.pem")
 }
 
-var MongoClient = require('mongodb').MongoClient;
-var database;
+// var MongoClient = require('mongodb').MongoClient;
+// var database;
 
 // Connection URL
-var url = 'mongodb://localhost:27017/';
+// var url = 'mongodb://localhost:27017/';
 // Use connect method to connect to the Server
 
-MongoClient.connect(url, function (err, client) {
-    if (err) {
-        console.log('failed to connect', err.message)
-        process.exit(1)
-    }
+// MongoClient.connect(url, function (err, client) {
+//     if (err) {
+//         console.log('failed to connect', err.message)
+//         process.exit(1)
+//     }
 
-    var db = client.db('traindiary');
+//     var db = client.db('traindiary');
     
-    const testEntry = {
-        date : Date.now(),
-        bpm : "150",
-        stats : "90"
-    }
+//     const testEntry = {
+//         date : Date.now(),
+//         bpm : "150",
+//         stats : "90"
+//     }
 
-    const entrys = db.collection('dates')
+//     const entrys = db.collection('dates')
     
     
-    // entrys.insertOne(testEntry, err => {
-    //     if(err) {
-    //         console.log('failed to write in DB')
-    //         process.exit(1)
-    //     }
-    //     console.log('Test sucessfully')
-    // })
+//     // entrys.insertOne(testEntry, err => {
+//     //     if(err) {
+//     //         console.log('failed to write in DB')
+//     //         process.exit(1)
+//     //     }
+//     //     console.log('Test sucessfully')
+//     // })
 
-    entrys.find({date: '1.1.11'}).toArray((err, documents) => {
-        if(err) {
-           console.log('failed to find!')
-           process.exit(1) 
-        }
-        console.log(documents)
-        let dbTestDate = JSON.stringify(documents)
-        app.get('/db', function (req, res) {
-            res.render('db', { title: 'dbTest', message: dbTestDate});
-         });
-        client.close()
-    })
+//     entrys.find({date: '1.1.11'}).toArray((err, documents) => {
+//         if(err) {
+//            console.log('failed to find!')
+//            process.exit(1) 
+//         }
+//         console.log(documents)
+//         let dbTestDate = JSON.stringify(documents)
+//         // app.get('/db', function (req, res) {
+            
+//         //  });
+//         client.close()
+//     })
 
-        // entrys.find({date: '1.1.11'}).toArray((err, documents) => {
-    //     if(err) {
-    //        console.log('failed to find!')
-    //        process.exit(1) 
-    //     }
-    //     console.log(documents)
-    // })
+//         // entrys.find({date: '1.1.11'}).toArray((err, documents) => {
+//     //     if(err) {
+//     //        console.log('failed to find!')
+//     //        process.exit(1) 
+//     //     }
+//     //     console.log(documents)
+//     // })
 
-    // database = db
-    // db.collection('dates').findOne({}, function (findErr, result) {
-    //     if (findErr) throw findErr;
-    //     console.log(result.date);
-    //     client.close();
-    // });
-});
-
-
-global.__basedir = __dirname
-
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
-
-app.use(express.static(path.join(__dirname, "public")))
-
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+//     // database = db
+//     // db.collection('dates').findOne({}, function (findErr, result) {
+//     //     if (findErr) throw findErr;
+//     //     console.log(result.date);
+//     //     client.close();
+//     // });
+// });
 
 
 http.createServer((req, res) => {
@@ -103,7 +91,3 @@ https.createServer(sslOpt, app).listen(sslPort, (err) => {
     console.log("ListenOnPort " + sslPort)
 })
 
-app.get('/', routes.getMetrolyse)
-app.get('/settings', routes.getSettings)
-app.get('/stuff', routes.getStuff)
-app.get('/traindiary', routes.getDb)
