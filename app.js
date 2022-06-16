fb2a08a4e238a94d71263508ed1d73677e686c2f
Timmy Schuetz <timotheusschuetz@outlook.de>
2022-06-16 16:26:37 +0200

    Database als API läuft das erste mal!

--- app.js
+++ app.js
@@ -1,82 +1,30 @@
 'use strict';
-const http = require('http')
-const https = require('https')
-const fs = require('fs');
-const path = require('path')
+const http = require('http'),
+    https = require('https'),
+    fs = require('fs')
 
-// const { exit } = require('process');
-const getApp = require('./lib/getApp')
-const app = getApp()
+const database = require('./lib/database'),
+    getApp = require('./lib/getApp'),
+    app = getApp()
+
 // const WebSocket = require("ws")
 
-
-const port = 80
-const sslPort = 443
-const sslOpt = {
-    key: fs.readFileSync("./ssl/privkey.pem"),
-    cert: fs.readFileSync("./ssl/cert.pem")
-}
-
-// var MongoClient = require('mongodb').MongoClient;
-// var database;
-
 // Connection URL
-// var url = 'mongodb://localhost:27017/';
-// Use connect method to connect to the Server
+var mongoUrl = 'mongodb://localhost:27017/';
 
-// MongoClient.connect(url, function (err, client) {
-//     if (err) {
-//         console.log('failed to connect', err.message)
-//         process.exit(1)
-//     }
+const port = 80,
+    sslPort = 443,
+    sslOpt = {
+        key: fs.readFileSync("./ssl/privkey.pem"),
+        cert: fs.readFileSync("./ssl/cert.pem")
+    }
 
-//     var db = client.db('traindiary');
-    
-//     const testEntry = {
-//         date : Date.now(),
-//         bpm : "150",
-//         stats : "90"
-//     }
 
-//     const entrys = db.collection('dates')
-    
-    
-//     // entrys.insertOne(testEntry, err => {
-//     //     if(err) {
-//     //         console.log('failed to write in DB')
-//     //         process.exit(1)
-//     //     }
-//     //     console.log('Test sucessfully')
-//     // })
-
-//     entrys.find({date: '1.1.11'}).toArray((err, documents) => {
-//         if(err) {
-//            console.log('failed to find!')
-//            process.exit(1) 
-//         }
-//         console.log(documents)
-//         let dbTestDate = JSON.stringify(documents)
-//         // app.get('/db', function (req, res) {
-            
-//         //  });
-//         client.close()
-//     })
-
-//         // entrys.find({date: '1.1.11'}).toArray((err, documents) => {
-//     //     if(err) {
-//     //        console.log('failed to find!')
-//     //        process.exit(1) 
-//     //     }
-//     //     console.log(documents)
-//     // })
-
-//     // database = db
-//     // db.collection('dates').findOne({}, function (findErr, result) {
-//     //     if (findErr) throw findErr;
-//     //     console.log(result.date);
-//     //     client.close();
-//     // });
-// });
+database.initialize(mongoUrl, err => {
+    if (err) {
+        console.log('Failed to connect to Database')
+    } 
+})
 
 
 http.createServer((req, res) => {
@@ -90,4 +38,3 @@ http.createServer((req, res) => {
 https.createServer(sslOpt, app).listen(sslPort, (err) => {
     console.log("ListenOnPort " + sslPort)
 })
-
