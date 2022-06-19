d05a6bfa25cccacdef0093379dfb9a036e11c719
Timmy Schuetz <timotheusschuetz@outlook.de>
2022-06-19 17:21:50 +0200

    StatusQuo

--- views/serviceworker.js
+++ views/serviceworker.js
@@ -1,27 +1,52 @@
+self.addEventListener('install', (e) => {
+    console.log('WORKER: install e in progress.');
+    e.waitUntil(
 
-const PRECACHE = 'precache-V1'
-
-const PRECACHE_URLS = [
-    '/',
-    '../app.js',
-    
-]
+      caches
 
 
-// Laden der Seite
-self.addEventListener('install',event => {
+        .open(1 + 'fundamentals')
+        .then(function(cache) {
 
-})
+          return cache.addAll([
+            '/',
+            '/css/style.css',
+            '/js/my.js',
+            '/app.js'
+          ]);
+        })
+        .then(function() {
+          console.log('WORKER: install completed');
+        })
+    );
+  });
 
-// Event wird gefeuert um Elemente zu verarbeiten.
-self.addEventListener('activate',event => {
-    
-})
+self.addEventListener('activate', () => activateSW());
+async function activateSW() {
+    console.log('WORKER: Activated');
+    const cacheKeys = await caches.keys();
 
+    cacheKeys.forEach(cacheKey => {
+        if (cacheKey !== getCacheName() ) {
+            caches.delete(cacheKey);
+        }
+    });
+}
 
-//Fängt Requests an den Server ab
-self.addEventListener('fetch',event => {
-    if(event.request.url("/users")){
-        event.request
+self.addEventListener('fetch', (e) => {
+  console.log(e.request);
+    if (e.request.method != 'GET') {
+        console.log("post");
+        
+    }else{
+        console.log("get");
+        
     }
-})
+    if(navigator.onLine){
+      if(e.request.url.pathname === "add"){
+        console.log("yea")
+      }
+    }
+
+    console.log('FETCH:' +e.request.url.pathname);
+});
